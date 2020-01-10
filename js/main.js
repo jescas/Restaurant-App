0cfb84750a7a3e18db47a1ae2a933317fce229e5
jess <jesdl604@hotmail.com>
2020-01-09 22:58:22 -0600

    Create service worker file on root folder. Register service worker on main.js.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@ let restaurants,
 var newMap
 var markers = []
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker
+    .register('./../sw.js')
+    .then(reg => console.log('ServiceWorker registration successful', reg))
+    .catch(err => console.log('ServiceWorker registration failed', err))
+  });
+};
+
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
