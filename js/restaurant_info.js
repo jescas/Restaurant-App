4e523e62a6219572a2baf2ad5b2b59d8ecbdc107
jess <jesdl604@hotmail.com>
2020-01-09 22:36:25 -0600

    Create new token and acquire new API key from mapbox.com. Implement in main.js and restaurant_info.js.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiamVzZGwiLCJhIjoiY2s1N25lM3V3MDVxNDNubzRtaHUyYTRqNyJ9.AjlqAzXP7OQWgZyat-5cFg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
